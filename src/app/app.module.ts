9af0812b44a97f30278fabd1c804c4f3f6e44590
ChethiyaNishanath <chethiyanishanath@gmail.com>
2020-05-09 14:41:57 +0530

    Set routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router'
 import { NgModule } from '@angular/core';
+
+import appRoutes from 'src/app/routes/app-routes';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -16,7 +19,8 @@ import { CategoryComponent } from './category/category.component';
     CategoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
