11106d4f75525e3a12fddee099495bb133b60736
ChethiyaNishanath <chethiyanishanath@gmail.com>
2020-05-09 14:36:13 +0530

    App routes

--- src/app/routes/app-routes.ts
+++ src/app/routes/app-routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
-import {LoginComponent} from '../login/login.component'
+import {LoginComponent} from '../login/login.component';
 import {RegisterComponent} from '../register/register.component';
+import {CategoryComponent} from '../category/category.component';
 
 
 const appRoutes:Routes=[
     {path:'register',component:RegisterComponent},
-    {path:'login',component:LoginComponent}
-]
+    {path:'login',component:LoginComponent},
+    {path:'category',component:CategoryComponent}
+];
+
+export default appRoutes;
