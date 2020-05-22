fff1883d1f6009d62b838c403e50ed1aa35d73e4
Kalpa <kalpafernando1998@gmail.com>
2020-05-22 22:05:23 +0530

    added properties to store jwt and userId

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,35 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../models/user.model";
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private _token: string;
+  private _authStatusListener = new Subject<boolean>();
+  private _isAuthenticated = false;
+  private tokenTimer;
+  private _userId: string;
   private url = 'http://localhost:3000'
+
+  get token() {
+    return this._token;
+  }
+
+  get authStatusListener() {
+    return this._authStatusListener.asObservable();
+  }
+
+  get isAuthenticated() {
+    return this._isAuthenticated;
+  }
+
+  get userId() {
+    return this._userId;
+  }
 
   constructor(private http: HttpClient) {
   }
