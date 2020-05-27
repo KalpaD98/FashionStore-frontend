36d8ce62c640c32a17751fe3fded4f4b313b24d0
Kalpa <kalpafernando1998@gmail.com>
2020-05-27 12:22:11 +0530

    added all-tems route and initialized item-list and item components

--- src/app/routes/app-routes.ts
+++ src/app/routes/app-routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from '../auth/login/login.component';
 import {CategoryComponent} from '../category/category.component';
 import {CreateItemComponent} from "../items/create-item/create-item.component";
+import {ItemListComponent} from "../items/item-list/item-list.component";
 
 
 const appRoutes: Routes = [
@@ -9,6 +10,7 @@ const appRoutes: Routes = [
   {path: 'category', component: CategoryComponent},
   {path: 'create-item', component: CreateItemComponent},
   {path: "edit/:itemId", component: CreateItemComponent},
+  {path: "all-items", component: ItemListComponent},
 ];
 
 export default appRoutes;
